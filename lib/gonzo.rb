ee6213fe11ebc229a713c14d99e87a4f113e7f11
mbauny <mbauny@users.noreply.github.com>
2020-05-09 17:30:49 +0200

    deletes obsolete Writer class

--- lib/gonzo.rb
+++ lib/gonzo.rb
@@ -2,18 +2,29 @@
 $LOAD_PATH << './lib'
 
 require 'gonzo/db'
-require 'gonzo/writer'
+require 'gonzo/index'
+
+class Gonzo
+  def run dir
+    db = Db.new dir
+    writeIndexFile((File.join dir, 'posts'), db.postsIndex.to_s)
+    writeIndexFile((File.join dir, 'tags'), db.tagsIndex.to_s)
+  end
+
+  def writeIndexFile dir, text
+    Dir.mkdir dir if !File.exists? dir
+    file = File.join dir, 'README.md'
+    File.write file, text
+  end
+end
 
 dir = ARGV.empty? ? Dir.pwd : ARGV[0]
 # TODO: use dir
 dir = '/Users/mbauny/sources/gonzo/test/samples'
 
 if File.directory? dir
-  db = Db.new dir
-  db.build
-
-  writer = Writer.new db, dir
-  writer.write
+  app = Gonzo.new
+  app.run dir
 else
   puts "[GONZO] Error #{dir} is not a directory."
 end
--- lib/gonzo/db.rb
+++ lib/gonzo/db.rb
@@ -1,30 +1,31 @@
 require 'gonzo/index'
+require 'gonzo/post'
+require 'gonzo/section'
 
 class Db
   attr_reader :postsIndex
   attr_reader :tagsIndex
 
   def initialize src_dir
-    @postsIndex = Index.new 'Posts'
-    @tagsIndex = Index.new 'Tags'
     @src_dir = src_dir
-  end
-  
-  def build
+    
     yearSections = Hash.new { |h, year| h[year] = Section.new year }
     tagsSections = Hash.new { |h, tag| h[tag] = Section.new(tag, :long) }
-
+    
     # Looking for post files...
     # TODO: Clean this loop
     Dir.glob("#{@src_dir}/posts/*.md").each do |file_path|
       next if file_path.end_with? 'README.md'
-
+      
       post = Post.new file_path
       yearSections[post.year] << post
       post.tags.each { |tag| tagsSections[tag] << post }
     end
-
+    
+    @postsIndex = Index.new 'Posts'
     yearSections.each { |k, section| @postsIndex << section }
+
+    @tagsIndex = Index.new 'Tags'
     tagsSections.each { |k, section| @tagsIndex << section }
   end
 end
