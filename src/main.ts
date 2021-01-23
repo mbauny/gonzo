d00b13c4e7cae316cd516c91d97823ca8f81f48b
mbauny <mbauny@users.noreply.github.com>
2021-01-23 23:35:30 +0100

    feat: add placeholder cli arg parsing

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,28 @@
-console.log('hello, world!')
+import * as yargs from 'yargs'
 
-export {}
+const args = yargs
+    .scriptName('gonzo')
+    .usage('Usage: $0 [options]')
+    .version()
+    .alias('v', 'version')
+    .help()
+    .alias('h', 'help')
+    .option('input', {
+        alias: 'i',
+        description: '',
+        demandOption: true,
+        type: 'string',
+    })
+    .option('output', {
+        alias: 'o',
+        default: '<input>',
+        description: '',
+        type: 'string',
+    })
+    .option('dry', {
+        alias: 'd',
+        description: '',
+    })
+
+console.log(args.argv)
+console.log('hello, world!')
