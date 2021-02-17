662a3212f6c9fcd8f1350b2a8e32a6a97123ab35
mbauny <mbauny@users.noreply.github.com>
2021-02-17 20:09:28 +0100

    test: add test for landing page output

---
+++ src/tests/output/catalog/landing.test.ts
@@ -0,0 +1,20 @@
+import { newDataBase } from '../../../database'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { getLanding } from '../../../output/write'
+
+describe('catalog', () => {
+    test('landing', () => {
+        const blogDir = './test/samples'
+        const db = newDataBase(blogDir)
+
+        expect(db).not.toEqual(undefined)
+
+        if (db) {
+            const readmePath = join(blogDir, 'README.md')
+            const utf8 = readFileSync(readmePath, 'utf-8')
+            const catalog = getLanding(db)
+            expect(catalog).not.toEqual(utf8)
+        }
+    })
+})
